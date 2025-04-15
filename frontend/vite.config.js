f1508ef1b1c9d12504bbada9eece91e757353845
timothy833 <ca9m8e5zy@gmail.com>
2025-04-15 13:04:37 +0800

    修正vite.config.js運作

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,13 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-
-// 自動判斷是否在 GitHub Pages 部署
-const isGitHubPages = process.env.IS_GITHUB_PAGES === 'true';
-const base = isGitHubPages ? '/fullstack-wordscape/' : '/';
-
 // https://vite.dev/config/
-export default defineConfig(()=>{
+export default defineConfig(({ mode })=>{
+  // 自動判斷是否在 GitHub Pages 部署
+  // ✅ 載入對應環境變數（例如 .env.production）
+  const env = loadEnv(mode, process.cwd(), '');
+
+  const isGitHubPages = env.IS_GITHUB_PAGES === 'true';
+  const base = isGitHubPages ? '/fullstack-wordscape/' : '/';
+  
   return {
     base,
     plugins: [react()]
