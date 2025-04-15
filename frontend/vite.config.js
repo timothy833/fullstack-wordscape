429bb3d5a134648b0d7411195c493a7d885e82c6
timothy833 <ca9m8e5zy@gmail.com>
2025-04-15 12:54:12 +0800

    修正vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 
 // 自動判斷是否在 GitHub Pages 部署
-const isGitHubPages = process.env.GITHUB_PAGES === 'true';
+const isGitHubPages = process.env.IS_GITHUB_PAGES === 'true';
 const base = isGitHubPages ? '/fullstack-wordscape/' : '/';
 
 // https://vite.dev/config/
